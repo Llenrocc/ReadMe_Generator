3b5d29717eaec95df881c39b19c6ea32e3e71ffe
LLenroc <nc_2@hotmail.com>
2020-08-08 16:51:49 -0400

    Adding questions to index.js

---
+++ .history/index_20200803184040.js
@@ -0,0 +1,33 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
+  // Code from develop folder
+  // array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
